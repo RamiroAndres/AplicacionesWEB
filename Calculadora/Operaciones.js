8e7b39e0f5c58b043cce9da7905bad83506e64d3
Ramiro <rb111002@ingenieria.sanmartin.edu.co>
2014-08-28 23:44:05 -0500

    Calculadora

---
+++ Calculadora/Operaciones.js
@@ -0,0 +1,8 @@
+
+function resultado(){
+	calc.screen.value = eval(calc.screen.value);
+}
+function borrar(screen){
+	calc.screen.value = calc.screen.value.substring(0,calc.screen.value.length-1)
+}
+
